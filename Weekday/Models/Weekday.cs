4e515a41d19254866ac1ba9b31188e3e9f34a019
Matt Stroud <lixingke06@gmail.com>
2020-05-13 14:41:51 -0700

    add code to ParseDate to extract the month and store it

--- Weekday/Models/Weekday.cs
+++ Weekday/Models/Weekday.cs
@@ -3,10 +3,18 @@ namespace Weekday.Models
   public class DayOfTheWeek
   {
     public string Date { get; set; }
+    public int Month { get; set; }
 
     public DayOfTheWeek(string date)
     {
       Date = date;
+    }
+
+    public void ParseDate()
+    {
+      string[] dateData = Date.Split("/");
+
+      Month = int.Parse(dateData[0]);
     }
   }
 }
